fa108e545500b2f9adf947546c67025d5660eeab
Gunner Bradley <gunnerbradley.dev@gmail.com>
2021-10-03 15:20:04 -0500

    week-8

---
+++ week-8/http-client.js
@@ -0,0 +1,18 @@
+export class HttpClient {
+    
+    async get(url, params = '') {
+        // New URL object.
+        let urlObject = new URL(url);
+
+        // Assigning url.search property to URLSearchParams object.
+        let paramsObject = new URLSearchParams(params);
+        urlObject.search = paramsObject
+
+        const res = await fetch(urlObject.toString(), {
+            method: "GET",
+        })
+
+        // Return res object as JSON.
+        return res.json();
+    }
+}
